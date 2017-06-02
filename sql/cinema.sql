33b563782cdd4c782dccaba31337d199d9d537f0
goobering <goobering@gmail.com>
2017-06-02 13:26:12 +0100

    Added customer and film tables to cinema.sql

--- sql/cinema.sql
+++ sql/cinema.sql
@@ -1,1 +1,14 @@
-require_relative '../db/sql_runner'
+DROP TABLE customers;
+DROP TABLE films;
+
+CREATE TABLE customers (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT8
+);
+
+CREATE TABLE films (
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT8
+);
