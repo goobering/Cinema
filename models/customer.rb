bb070a14007da930a8e1cb45ad0606ca87cd6f1f
goobering <goobering@gmail.com>
2017-06-02 13:22:51 +0100

    Added boilerplate methods to customer.rb, film.rb

--- models/customer.rb
+++ models/customer.rb
@@ -1,7 +1,30 @@
 class customer
 
-  def initialize()
-    
+attr_reader :id
+attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+    @funds = options['funds'].to_i()
+  end
+
+  def save()
+    sql = "INSERT INTO customers (name, funds) VALUES ('#{@name}', '#{@funds}' RETURNING id;"
+    customer = SqlRunner.run(sql).first()
+    @id = customer['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM customers;"
+    customers = SqlRunner.run(sql)
+    result = customers.map { |customer| Customer.new( customer ) }
+    return result
+  end
+
+  def self.delete_all()
+    sql = "DELETE * FROM customers;"
+    SqlRunner.run(sql)
   end
 
 end
